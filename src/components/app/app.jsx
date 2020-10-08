ceccd66bc12be776bda0709641f3e547b7fecd0f
UserGLA <tigr1488@yandex.ru>
2020-10-09 00:42:30 +0700

    7. Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+
 import Main from "../main/main";
+import Favorites from "../favorites/favorites";
+import Login from "../login/login";
+import Property from "../property/property";
 
 const App = (props) => {
   const {placesCount} = props;
 
   return (
-    <Main placesCount={placesCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main placesCount={placesCount} />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Property />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
